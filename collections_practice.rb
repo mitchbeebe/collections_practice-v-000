b823e94e167abcc5488c05727f5dacf718da37ad
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-04 02:41:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,12 +8,5 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    elsif a.length > b.length
-      -1
-    end
-  end
+    a.l
 end
